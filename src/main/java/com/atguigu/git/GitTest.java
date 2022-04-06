b23c3d7bbb8982b4c45305875c4644123ffb3e3e
Layne <Layne@atguigu.com>
2022-04-07 01:25:56 +0800

    second commit hot-fix

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git 2 ");
         System.out.println("hello fuck shit ");
         System.out.println("hello fuck shit 4");
+        System.out.println("hello fuck shit 5");
     }
 }
